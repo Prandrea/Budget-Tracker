39c1c49e29785e8d99c066bc49f5e75287aea525
prandrea <prandrea@utexas.edu>
2022-06-08 23:37:21 -0500

    finished idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -4,7 +4,7 @@ const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = ({ target }) => {
     const db = target.result;
-    db.createObjectStore("pending", { autoIncrement: true });
+    db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
 request.onsuccess = ({ target }) => {
@@ -14,3 +14,36 @@ request.onsuccess = ({ target }) => {
         checkDatabase();
     }
 };
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+  };
+
+  function saveRecord(record) {
+    const transaction = db.transaction([ "new_transaction"], "readwrite");
+    const store = transaction.objectStore("new_transaction");
+
+    store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction([ "new_transaction"], "readwrite");
+    const store = transaction.objectStore("new_transaction");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type" : "application/json"
+                }
+            })
+            .then (response => {
+                return response.json ();
+            })
+        }
+    }
+}
